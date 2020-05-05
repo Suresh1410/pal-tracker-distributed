979109b3e32beee74d31b91b25ce2776e69a738c
Suresh1410 <sureshkasinathan96@gmail.com>
2020-05-05 14:01:50 +0530

    Assignment 13

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -12,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.web.client.RestOperations;
 
 import java.util.TimeZone;
+
 @EnableWebSecurity
 @EnableResourceServer
 @EnableOAuth2Client
--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -9,8 +9,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import java.util.TimeZone;
+
 @EnableWebSecurity
 @EnableResourceServer
+@EnableOAuth2Client
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({
